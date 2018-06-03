1000c6e5abfa1e889f2a766145cd725c9c0e0b2f
RickSV <36182531+RickSV@users.noreply.github.com>
2018-06-02 20:02:39 -0500

    Add files via upload

---
+++ PaginaWeb2.0/src/paginaweb2/pkg0/JFrameWindow.java
@@ -0,0 +1,68 @@
+package paginaweb2.pkg0;
+
+public class JFrameWindow extends javax.swing.JFrame {
+
+    public JFrameWindow() {
+        initComponents();
+        SwingBrowser browser = new SwingBrowser();
+        browser.loadURL("https://ricksv.github.io/PAGINA/");
+        browser.setBounds(1, 1, jPanel1.getWidth() - 1, jPanel1.getHeight() - 1);
+        jPanel1.add(browser);
+    }
+
+    @SuppressWarnings("unchecked")
+     
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Java Web Browser");
+        setResizable(false);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1478, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 667, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JFrameWindow().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ PaginaWeb2.0/src/paginaweb2/pkg0/WebBrowserExample.java
@@ -0,0 +1,7 @@
+package paginaweb2.pkg0;
+public class WebBrowserExample {
+    public static void main(String[] args) {
+        JFrameWindow window = new JFrameWindow();
+        window.setVisible(true);
+    }
+}
